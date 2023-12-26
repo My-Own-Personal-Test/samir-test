651ec609ff1a19650562f71e879ab65a99b66706
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 20:21:06 +0700

    chores: base setup & install deps

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'require-await': 'error',
+    'no-empty-function': 'error',
+    'no-func-assign': 'error',
+    'n/prefer-global/process': ['off'],
+    'vue/custom-event-name-casing': ['error', 'camelCase'],
+    'vue/multi-word-component-names': ['error', {
+      ignores: [
+        'index',
+        'login',
+      ],
+    }],
+    'vue/singleline-html-element-content-newline': [
+      'error',
+      {
+        ignoreWhenNoAttributes: true,
+        ignoreWhenEmpty: true,
+      },
+    ],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'ignore',
+        multiline: 'below',
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
+    ],
+  },
+})
