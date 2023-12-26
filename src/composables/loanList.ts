05f850d3949e7a8a2ead99b67493ee954483d824
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 22:15:33 +0700

    feat: create error alert when fetch is failing

--- src/composables/loanList.ts
+++ src/composables/loanList.ts
@@ -2,16 +2,18 @@ import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import { fetchInstance } from '@/composables/fetch.config'
 import { useLoanListState } from '@/stores/loanListState'
+import { useAlertController } from '@/composables/ui/alertController'
 
 export const loadingList = ref(false)
 
 export async function getList() {
+  const { alertTrigger } = useAlertController()
   const loanList = useLoanListState()
   const { loanList: _loanList } = storeToRefs(loanList)
 
   loadingList.value = true
-  const res = await fetchInstance('/').catch((error) => {
-    console.log(error)
+  const res = await fetchInstance('/').catch((error: Error) => {
+    alertTrigger({ show: true, msg: error.message, variant: 'alert-error' })
   }).finally(() => {
     loadingList.value = false
   })
