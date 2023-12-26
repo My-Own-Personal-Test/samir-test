2e60200888d96509dbac341e7f2491c966866219
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 20:29:20 +0700

    chores: clean up boiler plate pinia

--- src/plugins/twMerge.ts
+++ src/plugins/twMerge.ts
@@ -1,8 +1,14 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-// import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
-
+/**
+ * Combines multiple class values using clsx and tailwind-merge utility functions.
+ * This function is designed to work with TypeScript.
+ *
+ * @param inputs - Class values to be combined.
+ * @returns A string representing the combined class values.
+ */
 export function cn(...inputs: ClassValue[]) {
+  // Combine class values using clsx and tailwind-merge
   return twMerge(clsx(inputs))
 }
