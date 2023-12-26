5bb290a3ff709b82f638a14dd7837244964ddc9f
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 20:59:11 +0700

    feat: add reusable skeleton component

---
+++ src/components/ui/atoms/SkeletonsComponents.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { cn } from '@/plugins/twMerge'
+
+interface Props {
+  class?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div :class="cn('skeleton', props.class)" />
+</template>
