36ec1fd49fa7b8e41bd9ad630db5c071695ce182
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 20:26:20 +0700

    chores: add date formatter plugins

---
+++ src/plugins/dateFormatter.ts
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs'
+
+/**
+ * Formats a given Date object using the dayjs library to a string in 'DD-MM-YYYY' format.
+ * @param {Date} date - The Date object to be formatted.
+ * @returns {string} - The formatted date string.
+ */
+export function dayFormat(date: Date): string {
+  return dayjs(date).format('DD-MM-YYYY')
+}
