5af43b0ba82df92ca1f37f7c484d5a8ac0a06239
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-26 20:45:28 +0700

    feat: add base button

---
+++ src/components/ui/atoms/BaseButton.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router'
+import { cn } from '@/plugins/twMerge'
+
+interface Props {
+  class: string | ''
+  id: string
+  disabled?: boolean
+  to?: string
+  target?: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button
+    v-if="!props.to"
+    :id="`${props.id}-button`"
+    :class="cn('btn', props.class)"
+    :disabled="props.disabled"
+  >
+    <slot />
+  </button>
+  <RouterLink
+    v-else
+    :id="`${props.id}-redirect-button`"
+    :to="props.to"
+    :class="cn('btn', props.class)"
+    :target="props.target"
+  >
+    <slot />
+  </RouterLink>
+</template>
