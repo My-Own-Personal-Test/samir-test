f7c761e99a99315fa49a926a80557774a96b32d7
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-27 12:37:30 +0700

    feat: user can now see loan detail on detail page

--- src/stores/loanListState.ts
+++ src/stores/loanListState.ts
@@ -1,10 +1,17 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { ref } from 'vue'
+import type { RouteParamValueRaw } from 'vue-router'
 
 export const useLoanListState = defineStore('loan_list', () => {
   const loanList = ref<Record<string, any>[]>([])
+  const loanDetail = ref<Record<string, any>>({})
 
-  return { loanList }
+  function getDetailById(loanId: RouteParamValueRaw | RouteParamValueRaw[]) {
+    const arr = loanList.value
+    loanDetail.value = arr.filter(item => item.id === loanId)
+  }
+
+  return { loanList, getDetailById, loanDetail }
 })
 
 if (import.meta.hot)
--- src/views/detailUser.vue
+++ src/views/detailUser.vue
@@ -1,3 +1,34 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useLoanListState } from '@/stores/loanListState'
+import { getList } from '@/composables/loanList'
+
+const loanList = useLoanListState()
+const { loanDetail: _loanDetail, loanList: _loanList } = storeToRefs(loanList)
+const route = useRoute()
+
+onMounted(async () => {
+  /**
+   * Conditionally fetches the loan list and retrieves the detail by ID.
+   * If the loan list has items, it directly calls getDetailById. Otherwise, it fetches the list first and then calls getDetailById.
+   *
+   * @param route - The route object containing parameters.
+   */
+  if (_loanList.value.length) {
+    loanList.getDetailById(route.params.loanId)
+  }
+
+  else {
+    await getList()
+    loanList.getDetailById(route.params.loanId)
+  }
+})
+</script>
+
 <template>
-  <h1>hello from user detail</h1>
+  <pre>
+    {{ _loanDetail }}
+  </pre>
 </template>
