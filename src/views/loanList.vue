c28f98c649264e49890082a90c9dead2877576e3
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-27 11:04:10 +0700

    feat: add mobile view loan list

--- src/views/loanList.vue
+++ src/views/loanList.vue
@@ -1,16 +1,48 @@
 <script lang="ts" setup>
+import { storeToRefs } from 'pinia'
 import { getList } from '@/composables/loanList'
+import { useLoanListState } from '@/stores/loanListState'
+import { useCurrencyFormat } from '@/plugins/numberFormatter'
 import BaseButton from '@/components/ui/atoms/BaseButton.vue'
+import CardRoot from '@/components/ui/organisms/CardRoot.vue'
+
+const loanList = useLoanListState()
+const { loanList: _loanList } = storeToRefs(loanList)
 </script>
 
 <template>
   <section class="grid place-items-center min-h-screen">
     <BaseButton
+      v-if="!_loanList.length"
       id="click2see"
       class="btn-primary"
       @click="getList"
     >
       Click to see Loan List
     </BaseButton>
+    <div
+      v-for="item in _loanList"
+      :key="item.id"
+      class="max-h-screen overflow-scroll w-96 p-4 sm:hidden"
+    >
+      <CardRoot
+        :user-id="item.borrower.id"
+      >
+        <template #head>
+          <p>Name: {{ item.borrower.name }}</p>
+        </template>
+        <template #body>
+          <div class="grid grid-rows-2 gap-y-1">
+            <div class="grid grid-cols-2">
+              <p>Amount: <br>{{ useCurrencyFormat(item.amount) }}</p>
+              <p>Rating: {{ item.riskRating }}</p>
+            </div>
+            <div class="grid grid-cols-2">
+              <p>Term: {{ item.term }} days</p>
+            </div>
+          </div>
+        </template>
+      </CardRoot>
+    </div>
   </section>
 </template>
