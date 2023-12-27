742f64a39380038f15d0eb0f8d0642bfa85ed8a2
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-27 16:50:38 +0700

    fix: race condition on the detail page

--- src/stores/loanListState.ts
+++ src/stores/loanListState.ts
@@ -2,16 +2,44 @@ import { acceptHMRUpdate, defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { RouteParamValueRaw } from 'vue-router'
 
-export const useLoanListState = defineStore('loan_list', () => {
+export const useLoanListState = defineStore('loan_state', () => {
   const loanList = ref<Record<string, any>[]>([])
   const loanDetail = ref<Record<string, any>>({})
+  /**
+   * this loading state default value is important to be true
+   * the sole purpose is to delay rendered card in the detail page.
+   * if the the default value is false, rendered detail and the loop will be in
+   * race condition
+   */
+  const loadingState = ref(true)
 
-  function getDetailById(loanId: RouteParamValueRaw | RouteParamValueRaw[]) {
+  /**
+   * Retrieves loan details by ID from the loan list.
+   *
+   * @param loanId - The ID of the loan to retrieve details for.
+   */
+  function getDetailById(loanId: RouteParamValueRaw | RouteParamValueRaw[]): void {
+    loadingState.value = true
     const arr = loanList.value
-    loanDetail.value = arr.filter(item => item.id === loanId)
+
+    for (let index = 0; index < arr.length; index++) {
+      if (arr[index].id === loanId) {
+        loanDetail.value = arr[index]
+        loadingState.value = false
+        return
+      }
+      else {
+        loadingState.value = false
+      }
+    }
   }
 
-  return { loanList, getDetailById, loanDetail }
+  return {
+    loanList,
+    getDetailById,
+    loanDetail,
+    loadingState,
+  }
 })
 
 if (import.meta.hot)
--- src/views/loanDetail.vue
+++ src/views/loanDetail.vue
@@ -1,34 +1,79 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onBeforeMount, onBeforeUnmount } from 'vue'
 import { useRoute } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { useLoanListState } from '@/stores/loanListState'
 import { getList } from '@/composables/loanList'
+import CardRoot from '@/components/ui/organisms/CardRoot.vue'
+import SkeletonsComponents from '@/components/ui/atoms/SkeletonsComponents.vue'
 
 const loanList = useLoanListState()
-const { loanDetail: _loanDetail, loanList: _loanList } = storeToRefs(loanList)
+const { loanDetail: _loanDetail, loanList: _loanList, loadingState } = storeToRefs(loanList)
 const route = useRoute()
 
-onMounted(async () => {
+onBeforeMount(async () => {
   /**
    * Conditionally fetches the loan list and retrieves the detail by ID.
    * If the loan list has items, it directly calls getDetailById. Otherwise, it fetches the list first and then calls getDetailById.
    *
    * @param route - The route object containing parameters.
    */
-  if (_loanList.value.length) {
-    loanList.getDetailById(route.params.loanId)
-  }
 
-  else {
+  if (!_loanList.value.length) {
     await getList()
     loanList.getDetailById(route.params.loanId)
   }
+  else {
+    loanList.getDetailById(route.params.loanId)
+  }
+})
+
+onBeforeUnmount(() => {
+  _loanDetail.value = {}
 })
 </script>
 
 <template>
-  <pre>
-    {{ _loanDetail }}
-  </pre>
+  <div class="p-10">
+    <SkeletonsComponents
+      v-if="loadingState"
+      class="h-96 w-full"
+    />
+
+    <CardRoot
+      v-else
+      :loan-id="route.params.loanId"
+      class="flex flex-col gap-y-4"
+    >
+      <template #head>
+        <div class="text-center">
+          <p class="text-2xl font-semibold">
+            Loan Detail
+          </p>
+          <p class="mt-2">
+            {{ route.params.loanId }}
+          </p>
+        </div>
+      </template>
+
+      <template #body>
+        <div class="sm:grid grid-cols-2 mt-6">
+          <div>
+            <p class="font-semibold">
+              Borrower Details:
+            </p>
+            <div class="mt-1">
+              <p>Name: {{ _loanDetail.borrower.name }}</p>
+              <p>Score: {{ _loanDetail.borrower.creditScore }}</p>
+            </div>
+          </div>
+          <div>
+            <p class="font-semibold">
+              Repayment Schedule:
+            </p>
+          </div>
+        </div>
+      </template>
+    </CardRoot>
+  </div>
 </template>
