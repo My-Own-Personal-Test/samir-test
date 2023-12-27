e28521ae0921d2c678e398200009e951ba349c6c
ryansyrl <m.ridwan.riansyah@gmail.com>
2023-12-27 12:51:03 +0700

    refactor: tweak card root component

--- src/components/ui/organisms/CardRoot.vue
+++ src/components/ui/organisms/CardRoot.vue
@@ -1,18 +1,24 @@
 <script lang="ts" setup>
 import { Icon } from '@iconify/vue'
+import { type RouteParamValueRaw, useRoute } from 'vue-router'
 import { cn } from '@/plugins/twMerge'
 import BaseButton from '@/components/ui/atoms/BaseButton.vue'
 
+type Params = RouteParamValueRaw | RouteParamValueRaw[]
 interface Props {
-  loanId: string
+  loanId: string | Params
   class?: string
 }
 
 const props = defineProps<Props>()
+const route = useRoute()
 </script>
 
 <template>
-  <div :class="cn('card w-full bg-base-100 shadow-lg', props.class)">
+  <div
+    :id="`card-id-${props.loanId}`"
+    :class="cn('card w-full bg-base-100 shadow-lg', props.class)"
+  >
     <div class="card-body">
       <slot name="head">
         head
@@ -20,7 +26,10 @@ const props = defineProps<Props>()
       <slot name="body">
         <p>If a dog chews shoes whose shoes does he choose?</p>
       </slot>
-      <div class="card-actions justify-end">
+      <div
+        v-if="route.name === 'home'"
+        class="card-actions justify-end"
+      >
         <BaseButton
           id="btn-redirect"
           class="btn-primary"
